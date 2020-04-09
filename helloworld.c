9fb7e34359e1a551385ce59dd72c797371019e58
eladman7 <eladman77@gmail.com>
2020-04-09 21:37:03 +0300

    returned helloworld to original code

--- helloworld.c
+++ helloworld.c
@@ -5,18 +5,7 @@
 int
 main(int argc, char *argv[])
 {
-  // printf(1, "Hello World Xv6\n");
-  // exit(0);
-  int status;
-    if (fork() == 0)
-        exit(5);
-    else
-        wait(&status);
-    if (status == 5)
-        printf(1, "Success!\n");
-    else
-        printf(1, "Fail :(\n");
-    printf(1, "Child exit status is %d\n", status);
-    exit(0);
+  printf(1, "Hello World Xv6\n");
+  exit(0);
 }
 
